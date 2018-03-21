b579f1cb61a6fbb5e3f481bd476aadfd6344d48b
Yang Naihua <naihua.yang@gmail.com>
2018-03-21 11:46:41 +0100

    fix

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -20,16 +20,16 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
 		context.getLogger().log("Input: " + input);
 		System.out.println("Processors: " + Runtime.getRuntime().availableProcessors());
         System.out.println("CommonPool Size: " + ForkJoinPool.commonPool().getPoolSize());
-        IntStream.rangeClosed(1, 10).parallel().forEach(integer -> {
-            System.out.println("Current Thread: " + Thread.currentThread()
-                + ", " + "CommonPool Size: " + ForkJoinPool.commonPool().getPoolSize());
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-            		
-                e.printStackTrace();
-            }
-        });
+//        IntStream.rangeClosed(1, 10).parallel().forEach(integer -> {
+//            System.out.println("Current Thread: " + Thread.currentThread()
+//                + ", " + "CommonPool Size: " + ForkJoinPool.commonPool().getPoolSize());
+//            try {
+////                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//     
+//                e.printStackTrace();
+//            }
+//        });
    
         
 		Map<String, String> headers = new HashMap<>();
